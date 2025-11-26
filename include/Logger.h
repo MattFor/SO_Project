55edde50f3fe362333bb5db808318b8ef6cbb02d
MattFor <matthew-forester@protonmail.com>
2025-11-26 13:00:23 +0100

    Initial Commit.

---
+++ include/Logger.h
@@ -0,0 +1,20 @@
+//
+// Created by MattFor on 26.11.2025.
+//
+
+#ifndef SO_PROJECT_LOGGER_H
+#define SO_PROJECT_LOGGER_H
+
+#include <mutex>
+#include <chrono>
+#include <string>
+#include <fstream>
+#include <iomanip>
+
+class Logger
+{
+
+};
+
+
+#endif //SO_PROJECT_LOGGER_H
---
+++ src/Logger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by MattFor on 26.11.2025.
+//
+
+#include "Logger.h"
